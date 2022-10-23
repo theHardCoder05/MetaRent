607db1dab2447c03bd11328f89c1928000b7371a
HardCoder <bitlabs27@gmail.com>
2022-10-23 09:27:51 +0900

    Add Smart Contracts and dependencies

---
+++ contracts/RicardianContract.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "hardhat/console.sol";
+
+/*
+      
+    * The Ricardian Smart Contract for MetaRent - Car Contract
+     
+   */
+
+contract CarContract {
+
+
+/** Constructor */
+constructor CarContract(){
+
+}
+
+
+}
---
+++ test/contract-test.js
@@ -0,0 +1,6 @@
+/* test/sample-test.js */
+describe("Ricardian Contract", function() {
+    it("Should create and execute market sales", async function() {
+      console.log('items: ', "HelloWorld...")
+    })
+  })
