ae443c1710d8deac290c24c69d17be0b3975524d
HardCoder <bitlabs27@gmail.com>
2022-10-26 09:30:41 +0900

    Add Solidity Smart Contract and images.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import { Flex, Text, Button, Tabs, TabList, Tab, TabPanels, TabPanel, Box, Link } from "@chakra-ui/react";
 import Head from "next/head";
+import { useEffect } from "react";
 import { useMoralis } from "react-moralis";
 import Header from "../components/Header";
 
 
 export default function Home() {
   const {isAuthenticated, user, isAuthenticating, authenticate, logout, isLoggingOut} = useMoralis()
+ 
+
+ useEffect(()=>{
+  console.log('hello from here...')
+  
+  
+ },[])
+
   if (!isAuthenticated) {
     return (
       <>
@@ -30,8 +39,8 @@ export default function Home() {
     <Flex direction="column" width="100vw" height="100vh">
       <Header isAuthenticated={isAuthenticated} isAuthenticating={isAuthenticating} user={user} authenticate={authenticate} logout={logout} isLoggingOut={isLoggingOut} />
       <Box flex="1" px="52" py="20">
-     
-        
+      loadimages()
+      <h1>asdfasdfsd</h1>
       
       </Box>
     </Flex>
